bb41d75f72d25ad90ebeddd0e46569fa49dc762f
邵竞帆 <alan@shaojingfandeMac-mini.local>
2025-03-03 22:58:33 +0800

    setup project, import vue-data-ui

---
+++ src/router/index.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/Dashboard/index.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ src/views/Dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <VueUiSparkbar :dataset="dataset" :config="config" style="width: 300px"/>
+</template>
+
+<script setup lang="ts">
+import {
+  VueUiSparkbar,
+  type VueUiSparkbarDatasetItem,
+  type VueUiSparkbarConfig
+} from "vue-data-ui";
+
+const dataset: VueUiSparkbarDatasetItem[] = [
+    {
+        name: "quality",
+        value: 61.953,
+        rounding: 2,
+        suffix: "%",
+    },
+    {
+        name: "popularity",
+        value: 20.0412,
+        rounding: 2,
+        suffix: "%",
+    },
+    {
+        name: "maintenance",
+        value: 33.3291,
+        rounding: 2,
+        suffix: "%",
+    },
+]
+const config: VueUiSparkbarConfig = {}
+</script>
